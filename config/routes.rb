2114e03e7abb0d7bd86601d9eadc6cabba40d3dc
Rafael Valim <rafaelvalim@globo.com>
2015-10-15 11:58:37 -0300

    Ajuste nos Javascripots do valuechain (removido da aplication.js)

--- config/routes.rb
+++ config/routes.rb
@@ -52,7 +52,7 @@ Rails.application.routes.draw do
   # match 'members/customer', controller: 'members', action: 'customer', via: :get, as
   match 'value_chains/classification/:id', controller: 'value_chains', action: 'classification', via: :get, as: :process_value_chain
   match 'process_modules/:id/get_list_ajax', controller: 'process_modules', action: 'get_list_ajax', via: :get, as: :list_ajax
-  match 'value_chains/get_classification/:id/:script_id', controller: 'value_chains', action: 'classification', via: :get, as: :classification_value_chain
+  match 'value_chains/set_classification/:id/:script_id', controller: 'value_chains', action: 'classification', via: :get, as: :classification_value_chain
   match 'requirements/destroy_ajax/:id', controller: 'requirements', action: 'destroy_ajax', via: :get
   match 'related_scripts/destroy_ajax/:id', controller: 'related_scripts', action: 'destroy_ajax', via: :get
   match 'value_chains/build/:id', controller: 'value_chains', action: 'build', via: :get, as: :build_value_chain
