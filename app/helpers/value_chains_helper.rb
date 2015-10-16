64054cc206924feab9128d54667554d7eea12bbd
Rafael Valim <rafaelvalim@globo.com>
2015-10-16 16:29:02 -0300

    Ajuste na classificação para melhoria da pesquisa

--- app/helpers/value_chains_helper.rb
+++ app/helpers/value_chains_helper.rb
@@ -5,31 +5,26 @@ module ValueChainsHelper
     ProcessModule.where(id: id).each do |pp1|
       next_id = pp1.referrer_process_module_id
       breadcrumb.push(pp1.description)
-      p "pp1 #{pp1.description}"
       loop do
         break if next_id.nil? || next_id == ''
         ProcessModule.where(id: next_id).each do |pp2|
           breadcrumb.push(pp2.description)
           next_id = pp2.referrer_process_module_id
-          p "pp2 #{pp2.description}"
         end
       end
     end
-    return breadcrumb
+    return breadcrumb.reverse
   end
 
   def show_process_chain(script_id)
-    values_chains = ValueChain.where(script_id: script_id)
+    value_chains = ValueChain.where(script_id: script_id)
     process_modules ||= Array.new
-    values_chains.each do |value_chain|
-      breadcrumb = get_breadcrumb_value_chain(value_chain.process_module_id)
-
-      breadcrumb.each do |b|
-        process_modules.push(b) unless process_modules.include?(b)
+    value_chains.each do |v|
+      if  ProcessModule.find_by_referrer_process_module_id(v.process_module_id).nil?
+        process_modules.push(get_breadcrumb_value_chain(v.process_module_id))
       end
-
     end
-    p process_modules
+    return process_modules
   end
 
 end
