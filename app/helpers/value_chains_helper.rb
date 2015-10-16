fb2b30246f317a78e69064a4c1860f2ec8f0e1fd
Rafael Valim <rafaelvalim@globo.com>
2015-10-16 13:58:27 -0300

    Ajuste na classificação para melhoria da pesquisa

--- app/helpers/value_chains_helper.rb
+++ app/helpers/value_chains_helper.rb
@@ -1,19 +1,35 @@
 module ValueChainsHelper
 
-  def get_breadcrumb_value_chain(process_module_id)
+  def get_breadcrumb_value_chain(id)
     breadcrumb ||= Array.new
-    ProcessModule.where('id = ? ', process_module_id).each do |pp1|
-      next_reference = pp1.referrer_process_module_id
-      breadcrumb.push("#{pp1.description}:#{pp1.id}")
+    ProcessModule.where(id: id).each do |pp1|
+      next_id = pp1.referrer_process_module_id
+      breadcrumb.push(pp1.description)
+      p "pp1 #{pp1.description}"
       loop do
-        break if next_reference.nil? || next_reference == ''
-        ProcessModule.where('id = ? ', next_reference).each do |pp2|
-          breadcrumb.push("#{pp2.description}:#{pp2.id}" )
-          next_reference = pp2.referrer_process_module_id
+        break if next_id.nil? || next_id == ''
+        ProcessModule.where(id: next_id).each do |pp2|
+          breadcrumb.push(pp2.description)
+          next_id = pp2.referrer_process_module_id
+          p "pp2 #{pp2.description}"
         end
       end
     end
-    return breadcrumb.reverse
+    return breadcrumb
+  end
+
+  def show_process_chain(script_id)
+    values_chains = ValueChain.where(script_id: script_id)
+    process_modules ||= Array.new
+    values_chains.each do |value_chain|
+      breadcrumb = get_breadcrumb_value_chain(value_chain.process_module_id)
+
+      breadcrumb.each do |b|
+        process_modules.push(b) unless process_modules.include?(b)
+      end
+
+    end
+    p process_modules
   end
 
 end
