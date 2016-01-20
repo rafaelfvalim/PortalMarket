21bcebed47d11a2d5d35666f723d83117d62e8da
rafaelfvalim <rafaelvalim@globo.com>
2016-01-20 18:48:10 -0200

    Ajuste no encriptitador de arquivos java

--- app/services/java_service.rb
+++ app/services/java_service.rb
@@ -13,14 +13,12 @@ class JavaService
     cmdout_read = ''
     cmdout_err = ''
     cmdout_din = ''
-    cmdout_wait = ''
 
     Dir.chdir(jar_path) do
       Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
         cmdout_read = stdout.read
         cmdout_err = stderr.read
         cmdout_din = stdin.read
-        cmdout_wait = wait_thr.read
       end
     end
     #return cmdout.gsub(/\n/,'') == 'sucess' ? true : false
