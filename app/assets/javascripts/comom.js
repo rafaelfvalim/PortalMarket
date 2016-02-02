c9b1f1be8535a204b6f7b4910cdd5a27a1bbed52
Rafael Valim <rafaelvalim@globo.com>
2016-02-02 14:44:50 -0200

    Tradução da Prices

--- app/assets/javascripts/comom.js
+++ app/assets/javascripts/comom.js
@@ -43,7 +43,7 @@ scrollPosition = function (container, scrollTo) {
 
 }
 
-var codeStore;
+var codeStore   ;
 codeStore = codeStore || (function () {
         var pleaseWaitDiv = $('<div class="modal fade bs-example-modal-sm" id="myPleaseWait" tabindex="-1"role="dialog" aria-hidden="true" data-backdrop="static"><div class="modal-dialog modal-sm"> <div class="modal-content"> <div class="modal-header"> <h4 class="modal-title"> <span class="glyphicon glyphicon-time"> </span>Please Wait </h4> </div> <div class="modal-body"> <div class="progress"> <div class="progress-bar progress-bar-infoprogress-bar-striped active"style="width: 100%"> </div> </div> </div> </div> </div> </div>');
         return {
