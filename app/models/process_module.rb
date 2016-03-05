064e8a6ab6b7b9e729a994db8ee0861e56de023c
Rafael Valim <rafaelvalim@globo.com>
2016-03-05 00:58:55 -0300

    Layout Klustter - site e area de usuário

--- app/models/process_module.rb
+++ app/models/process_module.rb
@@ -1,5 +1,5 @@
 class ProcessModule < ActiveRecord::Base
-  has_many :referrals, class_name: "ProcessModule" , foreign_key:  "referrer_process_module_id"
+  has_many :referrals, class_name: "ProcessModule" , foreign_key:  "referrer_process_module_id" , dependent: :destroy
   belongs_to :referring_process_module, class_name:  "ProcessModule"
   belongs_to :value_chain
   mount_uploader :image, ProcessModuleImageUploader
