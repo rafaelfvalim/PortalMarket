884ac20f17acd7fc068acd8fe404632449fbfb9e
Rafael Valim <rafaelvalim@globo.com>
2016-05-20 19:36:19 -0300

    Ajustes : https://trello.com/c/3vMZsj8x/43-nao-consegui-desistir-o-botao-veio-desabilitado Melhoramento na usabilidade desse grid

--- app/assets/javascripts/licenses.coffee
+++ app/assets/javascripts/licenses.coffee
@@ -1,3 +1,17 @@
+window.getDetailsFileName = (arquivo) ->
+  swal 'Nome do arquivo:', arquivo, 'info'
+
+window.getDetailsLicenseKey = (key) ->
+  swal 'Licença:', key, 'info'
+
+window.getDetails = (key, description, file_name, desistencia) ->
+  $('.modal-body #description').text(description)
+  $('.modal-body #key').text(key)
+  $('.modal-body #file_name').text(file_name)
+  $('.modal-body #desistencia').text(desistencia)
+  $('#modal_license').modal()
+
+
 $ ->
   $('#license-table').dataTable
     processing: true
@@ -5,4 +19,6 @@ $ ->
     ajax: $('#license-table').data('source')
     sPaginationType: 'full'
     bAutoWidth: true
-    columnDefs: [{orderable: false, targets: [7,8]}]
+#    columns: [{width: '5%' },{width: '5%' },{width: '5%' }, {width: '20%' }, {width: '10%' },{width: '20%' }]
+    columnDefs: [{orderable: false, targets: [6, 7, 8]}]
+
--- app/datatables/license_datatable.rb
+++ app/datatables/license_datatable.rb
@@ -7,6 +7,9 @@ class LicenseDatatable < AjaxDatatablesRails::Base
   def_delegator :@view, :cancel_license_licenses_path
   def_delegator :@view, :generate_key_licenses_path
   def_delegator :@view, :enableGenerateLicense
+  def_delegator :@view, :hidden_field_tag
+  def_delegator :@view, :content_tag
+  def_delegator :@view, :html_safe
 
   def sortable_columns
     # list columns inside the Array in string dot notation.
@@ -28,12 +31,19 @@ class LicenseDatatable < AjaxDatatablesRails::Base
           record.id,
           record.invoice_id,
           record.invoice.script_id,
-          "<small>#{record.description}</small>",
-          "<small>#{record.invoice.script_file}</small>",
-          "<small>#{record.license_key}</small>",
+          "<small>#{record.description} </small>",
+          button_tag(type: 'button', class: 'btn btn-success btn-xs', onclick: "getDetailsLicenseKey('#{record.license_key}')", disabled: !record.license_key.present?) do
+            content_tag(:strong, 'Chave da Licença')
+          end,
           record.expiration_date.nil? ? '' : record.expiration_date.strftime("%d/%m/%Y"),
+          if record.invoice.invoice_status_id == 5
+            '<i class="fa fa-times fa-2x" style="color: red;"></i>'.html_safe
+          end,
           link_to('Gerar licença', generate_key_licenses_path(id: record.id, invoice_id: record.invoice_id), :class => 'btn btn-info btn-xs', disabled: record.license_key.present? || record.invoice.invoice_status_id == 5, remote: true),
           link_to('Desisitir', cancel_license_licenses_path(invoice_id: record.invoice_id), class: 'btn btn-warning btn-xs ', disabled: enableGenerateLicense(record), remote: true),
+          button_tag(type: 'button', class: 'btn btn-success btn-xs', onclick: "getDetails('#{record.license_key}','#{record.description}','#{record.invoice.script_file}', '#{record.invoice.invoice_status_id == 5 ? 'Sim' : 'Não'}')") do
+            content_tag(:strong, 'Detalhes')
+          end,
       ]
     end
   end
