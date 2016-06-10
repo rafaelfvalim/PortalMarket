b3ec2183545195931638119e33dee75db72de268
Rafael Valim <rafaelvalim@globo.com>
2016-06-10 17:37:44 -0300

    Ajuste do envio do email de invoice

--- spec/mailers/previews/invoice_mail_preview.rb
+++ spec/mailers/previews/invoice_mail_preview.rb
@@ -1,6 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/invoice_mail
 class InvoiceMailPreview < ActionMailer::Preview
   def send
-    InvoiceMail.invoice_mail(User.find(1), Invoice.find(95))
+    InvoiceMail.invoice_mail(User.find(1), Invoice.find(1))
   end
 end
