4dccb7c51b3e992e198e48bd759d0b2c03b2aef3
rafaelfvalim <rafaelvalim@globo.com>
2016-06-16 17:03:15 -0300

    Ajuste de edição do avatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -148,10 +148,9 @@ class UsersController < ApplicationController
   end
 
   def upload_avatar
-    @user.avatar.cache_stored_file!
-    @user.avatar.recreate_versions!(:thumb, :circle_avatar)
-
     if @user.update(secure_params)
+      @user.avatar.cache_stored_file!
+      @user.avatar.recreate_versions!(:thumb, :circle_avatar)
       redirect_to edit_user_registration_path
     else
       redirect_to edit_user_registration_path, :alert => "Error :: Verifique o formato eo tamanho do arquivo"
