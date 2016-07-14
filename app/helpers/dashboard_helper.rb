8f7cc01611ab11eca17f3c5985c845b99a905c45
Rafael Valim <rafaelvalim@globo.com>
2016-07-14 18:30:00 -0300

    Ajuste de elemetos da dashboard responsivos

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -2,13 +2,13 @@ module DashboardHelper
 
   def widget_dash_board(bg_color, icon, header_description, header_count, link_path)
     html = <<-HTML
-    <div class="col-md-3point5">
+    <div class="col-md-3point5 col-xs-12 col-lg-4">
       <div class="widget style1 #{bg_color}">
         <div class="row">
-          <div class="col-xs-4">
+          <div class="col-xs-4 col-lg-4 col-md-4">
             <i class="#{icon}"></i>
           </div>
-          <div class="col-xs-8">
+          <div class="col-xs-8 col-lg-8 col-md-8">
             <div class="widget-text-header">
               <h4>#{header_description}</h4>
             </div>
@@ -17,7 +17,7 @@ module DashboardHelper
         </div>
         <hr>
         <div class="row">
-          <div class="col-xs-12 dash_links">
+          <div class="col-xs-12 col-lg-12 col-md-12 dash_links">
             <a href="#{link_path}" >
                 <span class="pull-left">#{t 'labels.buttons.details'}</span>
                 <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
